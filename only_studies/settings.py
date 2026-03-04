073e60af8f4253f3e1bfd768b18a152137c9a9f2
Veena S <veenasriramoju@gmail.com>
2026-03-04 21:18:09 +0530

    Switch to stable Django staticfiles backend

--- only_studies/settings.py
+++ only_studies/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise configuration for efficient static file serving
-_staticfiles_backend = 'whitenoise.storage.CompressedStaticFilesStorage'
+_staticfiles_backend = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Cache static files for 1 year (since they're cache-busted with hashes)
 WHITENOISE_MAX_AGE = 31536000  # 1 year in seconds
